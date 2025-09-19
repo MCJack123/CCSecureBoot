52068d492e384da716f2ccca0bd63c798cef2694
MCJack123 <jackmacwindowslinux@gmail.com>
2025-09-19 19:23:57 -0400

    Fixed typo

--- common/src/main/java/cc/madefor/phoenix/ccsecureboot/CCSecureBoot.java
+++ common/src/main/java/cc/madefor/phoenix/ccsecureboot/CCSecureBoot.java
@@ -4,7 +4,6 @@ import dan200.computercraft.api.ComputerCraftAPI;
 import dan200.computercraft.api.lua.IComputerSystem;
 import dan200.computercraft.api.lua.ILuaAPI;
 import dan200.computercraft.api.lua.ILuaAPIFactory;
-import net.minecraft.world.level.block.entity.JukeboxBlockEntity;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jspecify.annotations.Nullable;
